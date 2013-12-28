30161907b72b7d63da357242e2c9b4a12f4705eb
Sıtkı Bağdat <sbagdat@gmail.com>
2013-12-28 21:24:46 +0200

    Uygulama iskeletini oluştur

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsOnForum::Application.config.secret_key_base = 'fc13d3758c65e25bfa3fed56dc8b1c5dc8eadb1816f87cf5cc2cfb49d33ffa7acbe6c59a85d0b9ed849684c289ee4e3e717ae4e062675fe47710035e09b960bf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsOnForum::Application.config.session_store :cookie_store, key: '_rails-on-forum_session'
