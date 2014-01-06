098b6727f502f3a47f492c83ade9419de572f9d7
Sıtkı Bağdat <sbagdat@gmail.com>
2014-01-06 04:38:07 +0200

    Yeni kayıt işlemindeki hatayı düzelt

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,8 @@ module ApplicationHelper
       alert:  'info',
       error:  'warning' }[type]
   end
+
+  def form_params(parent, child)
+    child.new_record? ? [parent, child] : child
+  end
 end
