d223be3f0421b4b5bc516d98091f3b396b43f0b4
Sıtkı Bağdat <sbagdat@gmail.com>
2014-04-20 23:49:56 +0300

    secret_token.rb dosyasını sil, yerine secrets.yml dosyasını ekle

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 forums = ['Ruby ve Ailesi', 'Programlama Genel', 'Genel Konular', 'Otomobil Kulübü', 'Tasarım Genel', 'HTML&CSS']
 
 forums.each do |forum|
-  Forum.find_or_create_by_name(forum)
+  Forum.find_or_create_by(name: forum)
   puts "- #{forum} forumu oluşturuldu."
 end
